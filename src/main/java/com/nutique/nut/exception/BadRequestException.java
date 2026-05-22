964631583ab51e7c64c150c6d405ab2d76d094b1
Thewni03 <thewni2003@gmail.com>
2026-05-22 10:00:00 +0530

    Create BadRequestException

---
+++ src/main/java/com/nutique/nut/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.nutique.nut.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
